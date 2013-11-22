1e434a9e5f91e86feb0b21b53d8ad4f4b4e56ac4
Łukasz Gurdek <git@ukasiu.pl>
2013-11-22 23:11:54 +0100

    variuos fixes

--- includes/auth/auth_cas.php
+++ includes/auth/auth_cas.php
@@ -17,17 +17,18 @@ if ( !defined('IN_PHPBB') )
 
 include_once('CAS/CAS.php');
 
-phpCAS::client(CAS_VERSION_2_0, $config['cas_server'], (int) $config['cas_port'], $config['cas_uri']);
-phpCAS::setDebug();
+if($config['auth_method']=='cas') {
+    phpCAS::client(CAS_VERSION_2_0, (string) $config['cas_server'], (int) $config['cas_port'], (string) $config['cas_uri']);
+    //phpCAS::setDebug();
 
-if ($config['cas_validate'] == 0)
-  phpCAS::setNoCasServerValidation();
+    if ($config['cas_validate'] == 0)
+      phpCAS::setNoCasServerValidation();
 
-if (defined('IN_LOGIN') && request_var('mode', '') == 'login')
-{
-  phpCAS::forceAuthentication();
+    if (defined('IN_LOGIN') && request_var('mode', '') == 'login')
+    {
+      phpCAS::forceAuthentication();
+    }
 }
-
 
 /** 
 * Plugin initialization.
@@ -46,7 +47,6 @@ function autologin_cas()
         phpCAS::checkAuthentication();
     if (!phpCAS::isAuthenticated())
         return array();
-
     return get_user_row(phpCAS::getUser());
 }
 
@@ -62,6 +62,7 @@ function login_cas($username, $password)
         $user->session_kill();
         $user->session_begin();
     }
+
     phpCAS::forceAuthentication();
 
     if (phpCAS::isAuthenticated()) {
@@ -71,14 +72,16 @@ function login_cas($username, $password)
             $status = LOGIN_SUCCESS;
         }
     }
-    
     return array('status' => $status, 'error_msg' => $error_msg, 'user_row' => $user_row);
 }
 
 function logout_cas($user_row, $new_session)
 {
-    global $config;
+    session_start();
+    $_SESSION = array();
+    session_destroy();
     phpCAS::logout();
+
 }
 
 function validate_session_cas($user_row)
@@ -95,34 +98,34 @@ function validate_session_cas($user_row)
 */
 function acp_cas(&$new)
 {
-	global $user;
-	$user->add_lang('mods/auth_cas');
+    global $user;
+    $user->add_lang('mods/auth_cas');
 
-	$tpl = '
+    $tpl = '
 
-	<dl>
+    <dl>
           <dt><label for="cas_server">' . $user->lang['CAS_SERVER'] . ':</label><br /><span>' . $user->lang['CAS_SERVER_EXPLAIN'] . '</span></dt>
           <dd><code>https://</code><input type="text" id="cas_server" size="40" name="config[cas_server]" value="' . $new['cas_server'] . '" /></dd>
-	</dl>
-	<dl>
+    </dl>
+    <dl>
           <dt><label for="cas_port">' . $user->lang['CAS_PORT'] . ':</label><br /><span>' . $user->lang['CAS_PORT_EXPLAIN'] . '</span></dt>
           <dd><input type="text" id="cas_port" size="40" name="config[cas_port]" value="' . $new['cas_port'] . '" /></dd>
-	</dl>
-	<dl>
+    </dl>
+    <dl>
           <dt><label for="cas_uri">' . $user->lang['CAS_URI'] . ':</label><br /><span>' . $user->lang['CAS_URI_EXPLAIN'] . '</span></dt>
           <dd><input type="text" id="cas_uri" size="40" name="config[cas_uri]" value="' . $new['cas_uri'] . '" autocomplete="off" /></dd>
-	</dl>
-	<dl>
+    </dl>
+    <dl>
           <dt><label for="cas_validate">' . $user->lang['CAS_VALIDATE'] . ':</label><br /><span>' . $user->lang['CAS_VALIDATE_EXPLAIN'] . '</span></dt>
           <dd><input type="text" size="1" maxlength="1" id="cas_validate" name="config[cas_validate]" value="' . $new['cas_validate'] . '" /></dd>
-	</dl>
-	';
+    </dl>
+    ';
 
-	// These are fields required in the config table
-	return array(
-		'tpl'		=> $tpl,
-		'config'	=> array('cas_server', 'cas_port', 'cas_uri', 'cas_validate')
-	);
+    // These are fields required in the config table
+    return array(
+        'tpl'       => $tpl,
+        'config'    => array('cas_server', 'cas_port', 'cas_uri', 'cas_validate')
+    );
 }
 
 function get_user_row($username, $default_row = array(), $select_all = true)
@@ -131,9 +134,9 @@ function get_user_row($username, $default_row = array(), $select_all = true)
     $user_row = $default_row;
     $sql ='SELECT';
     if ($select_all)
-	$sql .= ' *';
+    $sql .= ' *';
     else
-	$sql .= ' user_id, username, user_password, user_passchg, user_email, user_type, user_style';
+    $sql .= ' user_id, username, user_password, user_passchg, user_email, user_type, user_style';
     $sql .= ' FROM ' . USERS_TABLE . "
         WHERE username_clean = '" . $db->sql_escape(utf8_clean_string($username)) . "'";
     $result = $db->sql_query($sql);
